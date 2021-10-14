abeacad0a82936c5e8b8152244b1ce6f492cc078
Martín Calderón <martincalderon92@outlook.com>
2021-10-14 15:36:57 -0300

    :sparkles: Add default image for games cards.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from '../styles/Card.module.scss';
+import gameDefault from '../img/gameDefault.png';
 
 export default function Card({ name, image, onClick }) {
 	return (
 		<div className={`${styles.card} ${styles.appearCard}`} onClick={onClick}>
 			<p>{name}</p>
 			<div className={styles.cardImage}>
-				<img src={image} alt={name} />
+				<img
+					className={!image && styles.defaultImage}
+					src={image || gameDefault}
+					alt={name}
+				/>
 			</div>
 		</div>
 	);
