ec4b5ddd3872ef9601a05f51b6fd3c7c842fcce4
Martín Calderón <martincalderon92@outlook.com>
2021-10-14 21:59:36 -0300

    :recycle: Update Platform Icons files.

--- src/components/PlatformIcons.js
+++ src/components/PlatformIcons.js
@@ -12,17 +12,31 @@ export default function PlatformIcons({ platforms }) {
 	let platformsNames = [];
 	let iconsToShow = [];
 
-	platforms?.map((item) => platformsNames.push(item.platform.name));
+	platforms?.map((item) => platformsNames.push(item.name));
 
 	const icons = {
 		PC: <FontAwesomeIcon key="pcIcon" icon={faDesktop} />,
 		PlayStation: <FontAwesomeIcon key="psIcon" icon={faPlaystation} />,
 		Xbox: <FontAwesomeIcon key="xboxIcon" icon={faXbox} />,
-		'Apple Macintosh': <FontAwesomeIcon key="appleIcon" icon={faApple} />,
+		Apple: <FontAwesomeIcon key="appleIcon" icon={faApple} />,
+	};
+
+	const platformsToIcons = {
+		'Microsoft Windows': icons['PC'],
+		Xbox: icons['Xbox'],
+		'Xbox 360': icons['Xbox'],
+		'Xbox One': icons['Xbox'],
+		macOS: icons['Apple'],
+		PlayStation: icons['Playstation'],
+		'PlayStation 2': icons['Playstation'],
+		'PlayStation 3': icons['Playstation'],
+		'PlayStation 4': icons['Playstation'],
+		'PlayStation 5': icons['Playstation'],
+		'PlayStation Portable': icons['Playstation'],
 	};
 
 	for (let i = 0; i < platformsNames.length; i++) {
-		iconsToShow.push(icons[platformsNames[i]]);
+		iconsToShow.push(platformsToIcons[platformsNames[i]]);
 	}
 
 	return <div className={styles.platformsIcons}>{iconsToShow}</div>;
