fcf645a044e69300aa99da2f5c6c440254e7b6ee
Martín Calderón <martincalderon92@outlook.com>
2021-10-15 17:59:38 -0300

    :sparkles: Add active page button.

--- src/components/PaginationButtons.js
+++ src/components/PaginationButtons.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/PaginationButtons.module.scss';
 import useFetch from '../hooks/useFetch';
 
 export default function PaginationButtons({ postsPerPage, paginate }) {
 	const fetchUrl = `https://trainee-gamerbox.herokuapp.com/games/count`;
 	const { data, loading } = useFetch(fetchUrl);
-
+	const [currentPage, setCurrentPage] = useState(1);
 	const pageNumbers = [];
 
 	for (let i = 1; i <= Math.ceil(data / postsPerPage); i++) {
 		pageNumbers.push(i);
 	}
 
+	const handlePageButtonClick = (pageNumber) => {
+		paginate(pageNumber);
+		setCurrentPage(pageNumber);
+	};
+
 	return (
 		<div className={styles.buttonsContainer}>
 			{!loading &&
 				pageNumbers?.map((pageNumber) => (
 					<button
-						className={styles.pageButton}
+						className={
+							styles.pageButton +
+							(currentPage === pageNumber ? ` ${styles.active}` : '')
+						}
 						key={`paginationButton${pageNumber}`}
-						onClick={() => paginate(pageNumber)}
+						onClick={() => handlePageButtonClick(pageNumber)}
 					>
 						{pageNumber}
 					</button>
