8b1c2e8248670aef31223acef6db10f330a53038
Martín Calderón <martincalderon92@outlook.com>
2021-10-14 22:53:15 -0300

    :sparkles: Create format date function.

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -1,3 +1,21 @@
+export const formatDate = (dateString) => {
+	const insertedDate = new Date(dateString);
+	const nowDate = Date.now();
+	const diffTime = Math.abs(nowDate - insertedDate);
+	const diffMinutes = Math.ceil(diffTime / (1000 * 60));
+	const diffHours = Math.ceil(diffTime / (1000 * 60 * 60));
+	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+	const diffMonths = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 30));
+
+	return diffMinutes < 61
+		? `${diffMinutes} minutes ago`
+		: diffHours < 25
+		? `${diffHours} hours ago`
+		: diffDays < 32
+		? `${diffDays} days ago`
+		: `${diffMonths} months ago`;
+};
+
 export const getNamesFromArray = (array) => {
 	return array?.map((item) => item.name).join(', ');
 };
