9399d82c56a99bded4943a162f90762cb3b038bf
Martín Calderón <martincalderon92@outlook.com>
2021-10-14 21:25:53 -0300

    :recycle: Isolate CardsContainer component.

---
+++ src/components/CardsContainer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from '../styles/CardsContainer.module.scss';
+import Spinner from './Spinner';
+import Card from './Card';
+
+export default function CardsContainer({ loading, posts, toDetail }) {
+	const handleToDetail = (postId) => {
+		toDetail(postId);
+	};
+
+	return (
+		<div className={styles.cardsContainer}>
+			{loading && <Spinner />}
+
+			{!loading &&
+				posts?.length > 0 &&
+				posts?.map((post) => (
+					<Card
+						key={post.id}
+						name={post.name}
+						image={post.cover_art?.formats?.small?.url}
+						onClick={() => handleToDetail(post.id)}
+					/>
+				))}
+		</div>
+	);
+}
