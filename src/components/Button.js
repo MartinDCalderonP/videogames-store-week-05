5fd5a2b7bde07b3bfe02292e23ae819832dd4e5b
Martín Calderón <martincalderon92@outlook.com>
2021-10-15 18:13:24 -0300

    :sparkles: Make button to accept children prop.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from '../styles/Button.module.scss';
 
-export default function Button({ className, onClick }) {
+export default function Button({ className, onClick, children }) {
 	return (
 		<button
 			className={styles.commentButton + (className ? ` ${className}` : '')}
 			onClick={onClick}
 		>
-			Comentar
+			{children}
 		</button>
 	);
 }
