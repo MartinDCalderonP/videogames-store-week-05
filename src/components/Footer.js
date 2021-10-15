51774995c72e939fdea7b9bc4c4ac76581d0f290
Martín Calderón <martincalderon92@outlook.com>
2021-10-15 20:22:40 -0300

    :fire: Delete extra console.log

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React, { memo } from 'react';
 import styles from '../styles/Footer.module.scss';
 
 export default memo(function Footer() {
-	console.log('RENDERING');
 	return (
 		<footer className={styles.footer}>
 			© Videogames Store 2021 - Todos los derechos reservados
