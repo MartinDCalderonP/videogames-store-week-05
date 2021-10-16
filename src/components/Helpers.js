ed697f070f97347399c638d1d61c150c593d5dd0
Martín Calderón <martincalderon92@outlook.com>
2021-10-15 23:49:39 -0300

    :recycle: Refactor format date function.

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -1,19 +1,22 @@
 export const formatDate = (dateString) => {
 	const insertedDate = new Date(dateString);
 	const nowDate = Date.now();
-	const diffTime = Math.abs(nowDate - insertedDate);
-	const diffMinutes = Math.ceil(diffTime / (1000 * 60));
-	const diffHours = Math.ceil(diffTime / (1000 * 60 * 60));
-	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-	const diffMonths = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 30));
+	const timeDifference = Math.abs(nowDate - insertedDate);
 
-	return diffMinutes < 61
-		? `${diffMinutes} minutes ago`
-		: diffHours < 25
-		? `${diffHours} hours ago`
-		: diffDays < 32
-		? `${diffDays} days ago`
-		: `${diffMonths} months ago`;
+	const differences = {
+		minutes: Math.ceil(timeDifference / (1000 * 60)),
+		hours: Math.ceil(timeDifference / (1000 * 60 * 60)),
+		days: Math.ceil(timeDifference / (1000 * 60 * 60 * 24)),
+		months: Math.ceil(timeDifference / (1000 * 60 * 60 * 24 * 30)),
+	};
+
+	return differences['minutes'] < 61
+		? `${differences['minutes']} minutes ago`
+		: differences['hours'] < 25
+		? `${differences['hours']} hours ago`
+		: differences['days'] < 32
+		? `${differences['days']} days ago`
+		: `${differences['months']} months ago`;
 };
 
 export const getNamesFromArray = (array) => {
