b419b7743521bd9f4632aca90ac382ed74089ed0
Martín Calderón <martincalderon92@outlook.com>
2021-10-16 21:41:40 -0300

    :recycle: Refactor setPageNumbers function and add useMemo.

--- src/components/PaginationButtons.js
+++ src/components/PaginationButtons.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import styles from '../styles/PaginationButtons.module.scss';
 import useFetch from '../hooks/useFetch';
-import { setPagesNumbers } from './Helpers';
 
 export default function PaginationButtons({ postsPerPage, paginate }) {
 	const fetchUrl = `https://trainee-gamerbox.herokuapp.com/games/count`;
 	const { data, loading } = useFetch(fetchUrl);
 	const [currentPage, setCurrentPage] = useState(1);
-	const pagesNumbers = setPagesNumbers(data, postsPerPage);
+	const pagesNumbers = React.useMemo(() => {
+		return Array.from(
+			{ length: Math.ceil(data / postsPerPage) },
+			(_, i) => 1 + i
+		);
+	}, [data, postsPerPage]);
 
 	const handlePageButtonClick = (pageNumber) => {
 		paginate(pageNumber);
