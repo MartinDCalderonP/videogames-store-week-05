f9c677bbbc11489ffb568e5ee62a77da5afad0d4
Martín Calderón <martincalderon92@outlook.com>
2021-10-18 08:49:05 -0300

    :sparkles: Add Sign In logic to modal.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -24,6 +24,43 @@ export default function Modal({ closeModal }) {
 		setPassword(e.target.value);
 	};
 
+	const handleSignInButton = async (e) => {
+		e.preventDefault();
+
+		let formData = {
+			identifier: username,
+			password: password,
+		};
+
+		try {
+			const response = await fetch(fetchUrl, {
+				method: 'POST',
+				body: JSON.stringify(formData),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+
+			const result = await response.json();
+
+			if (result.statusCode === 400) {
+				setToastMessage(result.message[0].messages[0].message);
+			} else {
+				setToastMessage(
+					`Welcome ${result.user.firstName} ${result.user.lastName}!`
+				);
+			}
+		} catch (err) {
+			setToastMessage(`${err}. Try again later.`);
+		}
+
+		setOpenToast(true);
+	};
+
+	const onCloseToast = () => {
+		setOpenToast(false);
+	};
+
 	return createPortal(
 		<div className={`${styles.overlay} ${styles.showModal}`}>
 			<div className={styles.modal}>
