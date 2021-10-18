d2540a01b5729c42ad36fe8e4f79686c9e54f32e
Martín Calderón <martincalderon92@outlook.com>
2021-10-17 23:20:58 -0300

    :sparkles: Add sign in button to open modal.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Navbar.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import Modal from './Modal';
 
 export default function Navbar({ toHome }) {
+	const [openModal, setOpenModal] = useState(false);
+
 	const handleHomeClick = () => {
 		toHome('home');
 	};
 
+	const handleSignInClick = () => {
+		setOpenModal(true);
+	};
+
+	const onCloseModal = () => {
+		setOpenModal(false);
+	};
+
 	return (
-		<nav className={styles.navbar}>
-			<div onClick={handleHomeClick}>
-				<FontAwesomeIcon icon={faHome} />
-				Home
-			</div>
-		</nav>
+		<>
+			<nav className={styles.navbar}>
+				<div onClick={handleHomeClick}>
+					<FontAwesomeIcon className={styles.home} icon={faHome} />
+					Home
+				</div>
+
+				<div onClick={handleSignInClick}>
+					Sign In
+					<FontAwesomeIcon className={styles.signIn} icon={faUserCircle} />
+				</div>
+			</nav>
+
+			{openModal && <Modal closeModal={onCloseModal} />}
+		</>
 	);
 }
